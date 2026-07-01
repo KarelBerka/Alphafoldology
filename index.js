d4dfc5e178d0100cc3cc24c0415431b8e93f2b68
Karel Berka <KarelBerka@users.noreply.github.com>
2026-07-01 19:55:42 +0200

    Format short citations on cards to <first author>, <Journal>, <Year> format and bust cache to v13

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let startX, startY;
 
   // Initialize Application
-  fetch('tools_data.json?v=12')
+  fetch('tools_data.json?v=13')
     .then(response => {
       if (!response.ok) throw new Error('Data file not found');
       return response.json();
