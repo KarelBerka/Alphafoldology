864d023a90fdbd258c00c64c099cb6e45d6b02ce
Karel Berka <KarelBerka@users.noreply.github.com>
2026-08-29 11:56:47 +0200

    Resolve issues #355-#320: add 15 tools (vBump, 1172 total)

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let startX, startY;
 
   // Initialize Application
-  fetch('tools_data.json?v=38')
+  fetch('tools_data.json?v=39')
     .then(response => {
       if (!response.ok) throw new Error('Data file not found');
       return response.json();
